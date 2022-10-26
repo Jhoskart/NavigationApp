13ecb5f7154175464b3b2396c78026ac69cb8a41
Jhoskar Toro <jhoskartoro@gmail.com>
2022-10-26 15:45:12 -0300

    se crea el repositorio

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { View, Text } from 'react-native'
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Text>Hello World</Text>
+    </NavigationContainer>
+  )
+}
